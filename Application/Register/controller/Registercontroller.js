04253935108621feb0f1b0d752cfc1daf54cb39e
yasin <ysndlklc1234@gmail.com>
2019-01-22 13:07:39 +0300

    register

--- Application/Register/controller/Registercontroller.js
+++ Application/Register/controller/Registercontroller.js
@@ -1,3 +1,47 @@
-aplication.controller("registerctrl", function ($scope,$location) {
-    $scope.mesaj="Merhaba Dünya";
-  });
+aplication.controller("registerctrl", function($scope, $location) {
+  $scope.registerdata = {
+    ad: "",
+    soyad: "",
+    email: "",
+    password: "",
+    cpassword: "",
+    phone: ""
+  };
+  $scope.checkRegisterForm = () => {
+    if (
+      $scope.registerdata.ad.trim() == "" ||
+      $scope.registerdata.ad.trim().length < 3
+    ) {
+      Component.showmessage("Uyarı", "İsim Alanı Geçersiz");
+    } else if (
+      $scope.registerdata.soyad.trim() == "" ||
+      $scope.registerdata.soyad.trim().length < 3
+    ) {
+      Component.showmessage("Uyarı", "SoyAd Alanı Geçersiz");
+    } else if (!Component.validateemail($scope.registerdata.email.trim())) {
+      Component.showmessage("Uyarı", "Email Adresi Geçersiz");
+    } else if (
+      $scope.registerdata.password.trim() == "" ||
+      $scope.registerdata.password.trim().length < 8
+    ) {
+      Component.showmessage("Uyarı", "Şifre Alanı Geçersiz");
+    } else if (
+      $scope.registerdata.cpassword.trim() == "" ||
+      $scope.registerdata.cpassword.trim().length < 8
+    ) {
+      Component.showmessage("Uyarı", "Şifre Alanı Geçersiz");
+    } else if (
+      $scope.registerdata.cpassword.trim() !=
+      $scope.registerdata.password.trim()
+    ) {
+      Component.showmessage("Uyarı", "Parolalar Eşleşmiyor");
+    } else if (
+      $scope.registerdata.phone.trim().length < 11 ||
+      $scope.registerdata.phone.trim() == ""
+    ) {
+      Component.showmessage("Uyarı", "Telefon Numrası Geçersiz");
+    }else {
+
+    }
+  };
+});
